e9b7389d3aa46b154239143301ffb534cffbc264
Lukehall6482 <Lukehall6482@gmail.com>
2024-09-11 17:04:48 +0100

    Add files via upload

---
+++ Learning python/Vairiables.py
@@ -0,0 +1,17 @@
+def monzo(job,salery):
+    print("monzo Should give me the", job, "job.")
+    print("they will pay me", salery)
+
+monzo("Apprentice software Engineer",88283773)
+monzo("Fraud investigator",89389328989328923898)
+monzo("Backend engineer" ,  1676766767)
+monzo( "Cop", 38283283893289)
+monzo( "Investigator", 388932819029390390910990)
+monzo("Senior", 9888999889)
+monzo( "Manager", 90329329239092)
+
+
+
+
+
+
